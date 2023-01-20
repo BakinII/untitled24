404c52ebf1beb81074dc5224a407325db2652e27
BakinII <bakin_ii@mail.ru>
2023-01-20 15:07:39 +0700

    1

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
-//#include <iostream>
-#include "table.h"
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    Tree dict;
-//формат даты год.месяц.день
-    dict.insert("22.05.11", "продам гараж","недвижемость","89235555555",1);
-    dict.insert("22.05.12","куплю доски","материалы","89235556789",0);
-    dict.insert("22.07.10", "продам гараж","недвижемость","89235551234",1);
-    dict.insert("22.06.05", "куплю машину","транспорт","89235555789",0);
-    dict.print();
-    std::cout<<dict.count<<std::endl;
-    dict.delldata("22.05.11");
-    dict.print();
-    return 0;
+п»ї#include "table.h"
 
+int main() {
+	Tree dict;
+	//С„РѕСЂРјР°С‚ РґР°С‚С‹ РіРѕРґ.РјРµСЃСЏС†.РґРµРЅСЊ-
+	dict.insert("22.05.11", "garage for sale", "real estate", "89235555555", 1);
+	dict.insert("22.05.12", "buy boards", "materials", "89235556789", 0);
+	dict.insert("22.07.10", "garage for sale", "real estate", "89235551234", 1);
+	dict.insert("22.06.05", "buy a car", "transport", "89235555789", 0);
+	dict.insert("22.03.10", "garage for sale", "real estate", "89235551234", 1);
+	dict.insert("22.01.05", "buy a car", "transport", "89235555789", 0);
+	dict.insert("22.04.10", "garage for sale", "real estate", "89235551234", 1);
+	dict.insert("22.12.05", "buy a car", "transport", "89235555789", 0);
+	dict.print();
+	std::cout << dict.count << std::endl;
+	dict.delldata("22.07.10");
+	dict.print();
+	dict.saveBinary("test.txt");
+    dict.searchrub("transport");
+	return 0;
 }
--- table.cpp
+++ table.cpp
@@ -3,18 +3,30 @@
 //
 
 #include "table.h"
-void Tree::insert(std::string a, std::string b,std::string c,std::string d,bool e){
-    count++;
-    if(!root) root=new Node(a,b,c,d,e);
-    else root->insert(a,b,c,d,e);
+#include <fstream>
+
+void Tree::insert(std::string a, std::string b, std::string c, std::string d, bool e) {
+	count++;
+	if (!root) root = new Node(a, b, c, d, e);
+	else root->insert(a, b, c, d, e);
 }
 
-void Tree::print(){
-    root->print();
+void Tree::print() {
+	root->print();
 }
+
 void Tree::delldata(std::string a) {
-    root->delldata(a);
+	root->delldata(a);
+    count--;
 }
-void Tree::searchrub(std::string a){
-    root->searchrub(a);
+
+void Tree::searchrub(std::string a) {
+	root->searchrub(a);
+}
+
+void Tree::saveBinary(std::string path) {
+	std::ofstream file;
+	file.open(path);
+	file << root->save();
+	file.close();
 }
